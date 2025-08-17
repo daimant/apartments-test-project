814d5f9ad1fe14cef975d4cbde001f691c1a5f30
Sergey Pomortsev <zaooooza@gmail.com>
2025-08-17 23:15:42 +0300

    added public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
-  css: ['~/assets/css/main.scss'],
+  css: ['~/assets/scss/main.scss'],
   app: {
     head: {
       link: [
